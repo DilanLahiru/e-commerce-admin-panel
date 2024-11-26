651e2291f7de172b27d13364f94ef1480c1f1c0e
DilanLahiru <dilanlahiruijse@gmail.com>
2024-11-26 17:19:53 +0530

    implement registration screen

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const Form = () => {
   return (
     <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-100">
-        <h1 className='text-5xl font-semibold font-sans'>Welcome Back</h1>
+        <h1 className='text-5xl font-semibold font-sans'>E-COM</h1>
         <p className='font-medium text-lg text-gray-500 mt-4'>Welcome back! Please enter your details.</p>
-        <div className="mt-8">
+        <div className="mt-5">
             <div className="">
-                <label className='text-lg font-medium'>Email</label>
-                <input type="text" placeholder='Enter your email' className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'/>
+                <label className='text-balance font-medium'>Email</label>
+                <input type="text" placeholder='Enter your email' className='w-full px-3 text-gray-700 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-violet-500'/>
             </div>
-            <div className="">
-                <label className='text-lg font-medium'>Password</label>
-                <input type="password" placeholder='Enter your Passowrd' className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'/>
+            <div className="mt-3">
+                <label className='text-balance font-medium'>Password</label>
+                <input type="password" placeholder='Enter your Passowrd' className='w-full px-3 text-gray-700 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-violet-500'/>
             </div>
-            <div className="mt-8 flex justify-between items-center">
+            <div className="mt-8 flex justify-between items-center w-full">
                 <div className="">
                     <input type="checkbox" id='remember'/>
                     <label for="remember" className='ml-2 font-medium text-base'>Remember for 30 days</label>
@@ -22,7 +22,7 @@ const Form = () => {
                 <button className='font-medium text-base text-violet-500'>Forgot Password</button>
             </div>
             <div className="mt-8 flex flex-col gap-y-4">
-                <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-medium'>
+                <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-violet-500 text-white text-lg font-medium'>
                     Sign in
                 </button>
             </div>
